f9ac9662f738a94c678146e0a4394f78cd1c2927
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2022-12-14 23:47:07 +0100

    add img and modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+gallery.addEventListener("click", imagesGallry)
+
+function imagesGallry(evnt) {
+
+  evnt.preventDefault()
+
+  const evntValue = evnt.target.nodeName
+  
+  if (evntValue !== "IMG") {
+    return console.log("не картинка")
+  }
+    const instance = basicLightbox.create(`
+    <img
+      class="gallery__image"
+      src=""
+      data-source="large-image.jpg"
+      alt=""
+      width="600"
+      heigh="600"
+
+    />
+`)
+instance.show()
+
+}
+
+
+const allImg = galleryItems.map(({ preview,original, description }) => 
+  `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join("")
+
+gallery.innerHTML = allImg
